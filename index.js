0d6ffab36fba0e555968985f343ba97bdfe2472e
IdanKfir <zunzer244@gmail.com>
2023-05-04 03:39:07 +0300

    completed-game-timers game-over

--- index.js
+++ index.js
@@ -120,6 +120,7 @@ const keys = {
   },
 };
 
+// colission detect if statements
 function rectangularCollision({ rectangle1, rectangle2 }) {
   return (
     rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
@@ -131,6 +132,34 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
   );
 }
+
+// determine winner
+function determineWnner({ player, enemy, timerId }) {
+  clearTimeout(timerId);
+  document.querySelector('#displayText').style.display = 'flex';
+  if (player.health === enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Tie';
+  } else if (player.health > enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 1 Wins';
+  } else if (enemy.health > player.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 2 Wins';
+  }
+}
+
+let timer = 60;
+let timerId;
+function decreaseTimer() {
+  if (timer > 0) {
+    timerId = setTimeout(decreaseTimer, 1000);
+    timer--;
+    document.querySelector('#timer').innerHTML = timer;
+  }
+  if (timer === 0) {
+    determineWnner({ player, enemy, timerId });
+  }
+}
+
+decreaseTimer();
 
 // main animation loop
 function animate() {
@@ -180,6 +209,11 @@ function animate() {
     enemy.isAttacking = false;
     player.health -= 20;
     document.querySelector('#playerHealth').style.width = player.health + '%';
+  }
+
+  //   end game based on health
+  if (enemy.health <= 0 || player.health <= 0) {
+    determineWnner({ player, enemy, timerId });
   }
 }
 
